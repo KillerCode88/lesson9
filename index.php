d1c01b08a8d63efd9422ae7f3d8e74caa6b48824
KillerCode88 <36702442+KillerCode88@users.noreply.github.com>
2018-03-27 10:28:10 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,48 +4,70 @@
 -->
 
 <?php
-class Car {
+
+class Car
+{
     private $color = 'white';
     private $wheelCount = 4;
-    public function repaint($newColor) {
+
+    public function repaint($newColor)
+    {
         $this->color = $newColor;
     }
-    public function getColor() {
+
+    public function getColor()
+    {
         return $this->color;
     }
-    public function setWheelCount($number) {
+
+    public function setWheelCount($number)
+    {
         $this->wheelCount = $number;
     }
 }
+
 $car1 = new Car;
 $car1->repaint('green');
 $car2 = new Car;
 $car2->setWheelCount(3);
-class TV {
+
+class TV
+{
     private $size;
     private $isSmart;
     private $price;
-    public function setPrice($newPrice) {
+
+    public function setPrice($newPrice)
+    {
         $this->price = $newPrice;
     }
-    public function __construct($size, $isSmart, $price) {
+
+    public function __construct($size, $isSmart, $price)
+    {
         $this->size = $size;
         $this->isSmart = $isSmart;
         $this->price = $price;
     }
 }
+
 $tv1 = new TV(40, true, 10);
 $tv1->setPrice(5);
 $tv2 = new TV(50, false, 7);
 $tv2->setPrice(5);
-class Pen {
+
+class Pen
+{
     private $color = 'blue';
     private $capacity = 10;
-    public function replace($newColor) {
+
+    public function replace($newColor)
+    {
         $this->color = $newColor;
         $this->capacity = 10;
     }
-    public function write() {
+
+    public function write()
+    {
         if ($this->capacity === 0) {
             echo 'У вас закончились чернила';
             exit;
@@ -53,64 +75,84 @@ class Pen {
         $this->capacity--;
     }
 }
+
 $pen1 = new Pen;
 $pen1->replace('black');
 $pen2 = new Pen;
 $pen2->write();
 $pen2->replace('red');
-class Duck {
+
+class Duck
+{
     private $gender;
     private $weight;
     private $wings = 2;
     private $alive = true;
-    public function shootWing() {
+
+    public function shootWing()
+    {
         $this->wings--;
     }
-    public function shoot() {
+
+    public function shoot()
+    {
         $this->alive = false;
     }
+
     public function __construct($gender, $weight)
     {
         $this->gender = $gender;
         $this->weight = $weight;
     }
 }
+
 $duck1 = new Duck('Селезень', 2);
 $duck1->shoot();
 $duck2 = new Duck('Утка', 2);
 $duck2->shootWing();
-class Goods {
+
+class Goods
+{
     private $name;
     private $price;
     private $discount = 0;
-    public function declareSale($discount) {
+
+    public function declareSale($discount)
+    {
         $this->discount = $discount;
         $newPrice = $this->price - ($this->price * $this->discount / 100);
         return $newPrice;
     }
+
     public function __construct($name, $price)
     {
         $this->name = $name;
         $this->price = $price;
     }
 }
+
 $commodity1 = new Goods('Печенье', 50);
 $commodity1->declareSale(10);
 $commodity2 = new Goods('Лимонад', 105);
 
-class News {
+class News
+{
     private $headline;
     private $text;
+
     public function __construct($array, $id)
     {
         $this->headline = $array[$id][0];
         $this->text = $array[$id][1];
     }
-    public function printNews() {
+
+    public function printNews()
+    {
         echo '<h3>' . $this->headline . '</h3>';
         echo '<p>' . $this->text . '</p>';
     }
 }
+
 $newsArray = [
     ['Заголовок', 'Новость'],
     ['Заголовок', 'Новость'],
@@ -127,7 +169,7 @@ $newsArray = [
 </head>
 <body>
 <?php
-foreach ($newsArray as $id=>$newsItem) {
+foreach ($newsArray as $id => $newsItem) {
     $item = new News($newsArray, $id);
     $item->printNews();
 }
